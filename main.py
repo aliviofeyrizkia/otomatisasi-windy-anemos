792b1556e245418b857013cfb872ad4c18721e25
aliviofeyrizkia <alivioitb@gmail.com>
2025-09-21 13:04:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from windy_scraper import capture_windy_screenshot
 
 def main():
-    url = "https://www.windy.com/-6.891/107.610"  # contoh lokasi: Dago Bandung
+    url = "https://www.windy.com/-6.935/107.606/gfs?gfs,rh,-7.331,107.606,9,m:dtBai8v"  # contoh lokasi: Dago Bandung
     image_path = capture_windy_screenshot(url, save_path="windy.png")
     print(f"[INFO] Screenshot tersimpan di: {image_path}")
 
